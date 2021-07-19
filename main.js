a48a089d5e8a60cb93fa0e61e0bf10328f0d3132
Krysia Masińska <krysiamasinska@gmail.com>
2021-07-19 22:47:29 +0200

    Clear error, show error

--- main.js
+++ main.js
@@ -6,9 +6,38 @@ const sendBtn = document.querySelector('.send');
 const clearBtn = document.querySelector('.clear');
 const popup = document.querySelector('.popup');
 
+const inputArray = [username, password, password2, mail];
+
+const showError = (input, msg) => {
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector('.error_text');
+    formBox.classList.add('error');
+    errorMsg.textContent = msg;
+};
+
+const clearError = input => {
+    const formBox = input.parentElement;
+    formBox.classList.remove('error');
+}
+
+const checkForm = input => {
+    input.forEach(el => {
+        if (el.value === '') {
+            showError(el, el.placeholder);
+        } else {
+            clearError(el);
+        }
+    })
+};
+
+sendBtn.addEventListener('click', e => {
+    e.preventDefault();
+    checkForm(inputArray);
+});
+
 clearBtn.addEventListener('click', e => {
     e.preventDefault();
-    [username, password, password2, mail].forEach(el =>
+    inputArray.forEach(el =>
         el.value = ''
     );
 });
